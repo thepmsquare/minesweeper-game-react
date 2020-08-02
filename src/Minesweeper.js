ff4987f5ff3e8da21541ac261219a95917ad786d
thePmSquare <impmmmm@gmail.com>
2020-08-02 08:44:56 +0530

    setup files for minesweeper

---
+++ src/Minesweeper.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./stylesheets/Minesweeper.css";
+
+class Minesweeper extends Component {
+  render = () => {
+    return (
+      <div className="Minesweeper">
+        <h1>Minesweeper</h1>
+      </div>
+    );
+  };
+}
+
+export default Minesweeper;
