f2388457d6717fbf8ebf2f7d7d2693a47edf0116
thePmSquare <impmmmm@gmail.com>
2020-08-19 01:44:52 +0530

    Updates

---
+++ precache-manifest.206ef87628d31d2397fcca842b6137f6.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a9c1f336a8f5ce4c0d1c983e916b5c",
+    "url": "/minesweeper-game-react/index.html"
+  },
+  {
+    "revision": "d83aa722ca273ad21aea",
+    "url": "/minesweeper-game-react/static/css/2.a3a84f15.chunk.css"
+  },
+  {
+    "revision": "599a57363fca6bf1a9db",
+    "url": "/minesweeper-game-react/static/css/main.94d23040.chunk.css"
+  },
+  {
+    "revision": "d83aa722ca273ad21aea",
+    "url": "/minesweeper-game-react/static/js/2.92be98d6.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/minesweeper-game-react/static/js/2.92be98d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "599a57363fca6bf1a9db",
+    "url": "/minesweeper-game-react/static/js/main.a6c64366.chunk.js"
+  },
+  {
+    "revision": "b6d3ce3f909e865d38bd",
+    "url": "/minesweeper-game-react/static/js/runtime-main.8f58ab42.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/minesweeper-game-react/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/minesweeper-game-react/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/minesweeper-game-react/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/minesweeper-game-react/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/minesweeper-game-react/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/minesweeper-game-react/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/minesweeper-game-react/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/minesweeper-game-react/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/minesweeper-game-react/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/minesweeper-game-react/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/minesweeper-game-react/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/minesweeper-game-react/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/minesweeper-game-react/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/minesweeper-game-react/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/minesweeper-game-react/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/minesweeper-game-react/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
